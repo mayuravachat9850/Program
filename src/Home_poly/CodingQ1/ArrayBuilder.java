41609b113d359b1e87ce04e0fc761f98a66d16eb
Mayur Avachat <87423951+mayuravachat9850@users.noreply.github.com>
2021-07-24 17:37:19 +0530

    Commit Q2

---
+++ src/Home_poly/CodingQ1/ArrayBuilder.java
@@ -0,0 +1,28 @@
+package Home_poly.CodingQ1;
+
+public class ArrayBuilder {
+    public static int[] getArray(int a, int b) {
+        int[] ex = {a,b};
+        return ex;
+    }
+    public static int[] getArray(int a, int b, int c) {
+        int[] ex = {a,b,c};
+        return ex;
+    }
+    public static int[] getArray(int a, int b, int c,int d) {
+        int[] ex = {a,b,c,d};
+        return ex;
+    }
+    /*
+     * TODO 1: Provide three overloaded methods as shown below:
+     * 		+ getArray(a: int, b: int) : int[]
+     * 		+ getArray(a: int, b: int, c: int) : int[]
+     * 		+ getArray(a: int, b: int, c: int, d: int) : int[]
+     *
+     * 		1. All three methods should be of public and static type.
+     * 		2. All three methods should be named getArray().
+     * 		3. All three methods should take some int arguments and return an array
+     * 			containing those arguments.
+     */
+
+}
---
+++ src/Home_poly/CodingQ1/Ex1.java
@@ -0,0 +1,11 @@
+package Home_poly.CodingQ1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int[] arr = ArrayBuilder.getArray(10, 20, 30);
+        for (int ele: arr) {
+            System.out.println(ele);
+        }
+    }
+}
+
