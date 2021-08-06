ae697bb12b17a2d8cda9daa9fbec0561c1550565
Mayur Avachat <87423951+mayuravachat9850@users.noreply.github.com>
2021-08-06 20:01:57 +0530

    Delete Last Node of Linked List

---
+++ src/LinkedList/Code1.java
@@ -0,0 +1,53 @@
+package LinkedList;
+/*
+Delete Last Node of Linked List
+*/
+import java.util.*;
+
+public class Code1 {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int n) {
+            data = n;
+            next = null;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        head = null;
+        if (n > 0) {
+            head = new Node(s.nextInt());
+            Node temp = head;
+            for (int i = 1; i < n; i++) {
+                temp.next = new Node(s.nextInt());
+                temp = temp.next;
+            }
+        }
+        deleteLastNode();
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    // Method to delete the last node of the LinkedList
+    static void deleteLastNode() {
+        if (head == null)
+            return;
+        else if (head.next == null) {
+            head = null;
+        } else {
+            Node temp = head;
+            while (temp.next.next != null)
+                temp = temp.next;
+            temp.next = null;
+        }
+    }
+}
+
