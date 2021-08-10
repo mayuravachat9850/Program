6f6fd49a0c101077fd8e90fd84ed93b2e8961525
Mayur Avachat <87423951+mayuravachat9850@users.noreply.github.com>
2021-08-10 21:51:32 +0530

    add

---
+++ src/NEWqueue/Reverse.java
@@ -0,0 +1,34 @@
+package NEWqueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+class Reverse {
+    static Queue<Integer> q = new LinkedList<Integer>();
+    static Stack<Integer> s = new Stack<>();
+
+    public static void func() {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            s.push(q.remove());
+        }
+        for (int i = 0; i < size; i++) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void main(String arg[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int j = 0; j < n; j++) {
+            q.add(in.nextInt());
+        }
+        func();
+        System.out.println(q);
+    }
+}
+
+
